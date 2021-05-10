f44c7f755916d16a87e3ef02843204c3fb7be7f2
jkolodziej <julkaa.k@gmail.com>
2021-05-10 18:01:34 +0200

    Add JWT utils and security constants class

---
+++ src/main/java/com/zzpj/security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.zzpj.security;
+
+public class SecurityConstants {
+
+    public static final String SECRET = "Yl0e-ulkfUI06IAqxdJoHXYxcw-mIl5pj1-SrNycbvktlYTINhRABKGDL_8b7hx7xBgZ5E3jNpiO422AV2q97glCd2n8nbYSSiAbus7gI6Qz0YnTZf0ZlqU66v5XqG4S1gECTLqvWBUfap3d-GWFSSAYZjbuhRd-_L3RceGXpR8";
+    public static final long JWT_TIMEOUT = 15 * 60 * 1000;
+    public static final String ISSUER = "wzas03";
+    public static final String BEARER = "Bearer ";
+}
