982ffc1fdc429b803107ae418f77376d342b2346
Xarria <224340@edu.p.lodz.pl>
2021-05-16 22:28:22 +0200

    push test

--- src/main/java/com/zzpj/services/interfaces/AccountServiceInterface.java
+++ src/main/java/com/zzpj/services/interfaces/AccountServiceInterface.java
@@ -1,5 +1,6 @@
 package com.zzpj.services.interfaces;
 
+import com.zzpj.exceptions.AccountDoesNotExistException;
 import com.zzpj.exceptions.EmailAlreadyExistsException;
 import com.zzpj.exceptions.LoginAlreadyExistsException;
 import com.zzpj.model.entities.Account;
@@ -10,13 +11,13 @@ public interface AccountServiceInterface {
 
     void createAccount(Account account) throws LoginAlreadyExistsException, EmailAlreadyExistsException;
 
-    Account getAccountByLogin(String login);
+    Account getAccountByLogin(String login) throws AccountDoesNotExistException;
 
     List<Account> getAllAccounts();
 
-    void updateAccount(String login, Account updatedAccount);
+    void updateAccount(String login, Account updatedAccount) throws AccountDoesNotExistException;
 
-    void activateAccount(String login);
+    void activateAccount(String login) throws AccountDoesNotExistException;
 
-    void deactivateAccount(String login);
+    void deactivateAccount(String login) throws AccountDoesNotExistException;
 }
