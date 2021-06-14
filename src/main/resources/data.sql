ef22a8873fe95be598a4558d48333ec2300251c2
jkolodziej <julkaa.k@gmail.com>
2021-06-14 21:31:56 +0200

    Fix recipe details view, update styles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -51,4 +51,12 @@ INSERT INTO Recipe_Ingredient (recipe_id, ingredient_id)
 VALUES (1, 1),
        (1, 2),
        (1, 3),
-       (1, 4);
+       (1, 4),
+       (2, 5),
+       (2, 6),
+       (2, 1),
+       (2, 2),
+       (2, 3),
+       (3, 1),
+       (3, 5);
+
